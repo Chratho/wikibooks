b4627259c5b462ecbe0357c8c64fdea9027d9446
Christian Thomas <chr.thomas@gmx.at>
2015-07-20 16:39:11 -0400

    Add import script for mongo-db

---
+++ xml_to_mongo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3.4
+import xml.etree.ElementTree as etree
+
+import os
+from pymongo import MongoClient
+
+XML_DATA_SRC = os.path.join("xml","enwikibooks.xml")
+
+STEP_SIZE = 50
+
+conn = None
+try:
+    conn = MongoClient()
+except:
+    print("Error: Unable to connect to database.")
+    sys.exit(-1);
+col = conn.wikibooks.books
+
+tree = etree.parse(XML_DATA_SRC,parser=etree.XMLParser(encoding="UTF-8")).getroot()
+
+i = 0
+dicts = []
+for node in tree:
+    if node.tag != "doc": continue
+
+    book_id = node.attrib['id']
+    url = node.attrib['url']
+    title = node.attrib['title']
+
+    dicts.append({ "book_id": book_id, "url": url, "title": title, "text": node.text })
+
+    i += 1
+    if (i % STEP_SIZE == 0):
+        col.insert_many(dicts)
+        dicts = []
